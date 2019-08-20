4df55df54c0845f56ca5c1c1f365d61dd6e9a043
Ivan Alexandrov <smruck@gmail.com>
2019-08-20 23:34:18 +0300

    changes on UI, Services pages added

---
+++ HPowerTunings.Web/Controllers/SiteViewsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HPowerTunings.Web.Controllers
+{
+    public class SiteViewsController : Controller
+    {
+        public IActionResult ServicesBodywork()
+        {
+            return View();
+        }
+
+        public IActionResult ServicesDiagnostics()
+        {
+            return View();
+        }
+
+        public IActionResult ServicesRestoration()
+        {
+            return View();
+        }
+
+        public IActionResult ServicesRepair()
+        {
+            return View();
+        }
+    }
+}
